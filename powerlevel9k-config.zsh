d1af6a1c86a7d8a075db677abbfa86e5686ef318
blyndusk <alexandredelaloydev@gmail.com>
2019-03-19 11:53:53 +0100

    🔧 add pl9k new template

--- powerlevel9k-config.zsh
+++ powerlevel9k-config.zsh
@@ -1,21 +1,44 @@
 
+source  ~/.oh-my-zsh/custom/themes/powerlevel9k/powerlevel9k.zsh-theme
+
+
 # LEFT SIDE
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-    disk_usage ip context dir dir_writable 
-    newline load node_version nvm vcs
+    context 
+    ip 
+    disk_usage 
+    newline 
+ 
+    node_version
+    dir 
+    dir_writable
+    load 
+    vcs
 )
 
 # RIGHT SIDE
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status newline history time date battery)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
+    status    
+    history
+    newline
+    time
+    date
+    battery
+)
 
 # ROOT ICON 
 POWERLEVEL9K_HOME_ICON='🌳'
 
+
 # BATTERY STAGES
-# POWERLEVEL9K_BATTERY_STAGES="▁▂▃▄▅▆▇█" 
 POWERLEVEL9K_BATTERY_STAGES="😡😩🙁😐🙂😌😄🤩"
 
 # BATTERY CHARGING LEVELS
 POWERLEVEL9K_BATTERY_LEVEL_BACKGROUND=(235 237 239 241 243 245 247 251)
 
-POWERLEVEL9K_BACKGROUND_JOBS_VERBOSE_ALWAYS="true"
+# #794A98: purple // 055
+# #98e014: green // 154
+# #c10b29: red // 124
+# #E9F4F7: white // 231
+
+# POWERLEVEL9K_CONTEXT_DEFAULT_BACKGROUND="055"
