c73010c670023e3aea34f87716f0406e10c48c14
blyndusk <alexandredelaloydev@gmail.com>
2019-03-20 18:01:10 +0100

    :wrench: solve acp conflicts

--- scripts/add-commit-push.sh
+++ scripts/add-commit-push.sh
@@ -3,10 +3,7 @@
 MESSAGE=$1
 
 git add .
-<<<<<<< HEAD 
 echo Enter your commit message:
 read MESSAGE
-=======
->>>>>>> parent of 1e7953a... :wrench: update acp command
 git commit -m "$MESSAGE"
 git push
