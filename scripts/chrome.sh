01eea65b5ec0bd586e4c9bcb0668074ba8b9f3bb
blyndusk <alexandredelaloydev@gmail.com>
2019-03-22 15:24:05 +0100

    💡 add doc to source code & create update functions

--- scripts/chrome.sh
+++ scripts/chrome.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+# ===============================================
+# ------------------ VARIABLES ------------------
+# ===============================================
 
-# Arrays of urls
+# tabs url arrays
 DEV_URLS=(
     "https://github.com"
     "https://gitstalk.netlify.com"
 )
-
 MAIL_URLS=(
     "https://mail.google.com/"
     "https://outlook.live.com/"
@@ -22,7 +24,8 @@ SOCIAL_URLS=(
     "https://messenger.com"
 )
 
-LISTS=(
+# YouTube Music & Youtube playlists
+PLAYLISTS=(
     # grandson
     "PL5UDQMgl1JYhU8PD-3GudN_twxxuGp6y2"
     # to sleep
@@ -35,35 +38,47 @@ LISTS=(
     "axOhJh1x2n8"
 )
 
-# will update the dev urls if user param was given
+# https://youtube.com/$VID/video_id
+VID="watch?v="
+# https://youtube.com/$LIST/playlist_id
+MUSIC="playlist?list="
+
+# First parameter
+PARAM=$1
+
+
+# ===============================================
+# ------------------ FUNCTIONS ------------------
+# ===============================================
+
+# will open new tabs in chrome with given arguments
+CHR () { open -a "Google Chrome" $* ; }
+
+# set $MUSICVIDS_URLS array with new value; 
+# $1 is the $MUSICVIDS_URLS index,
+# $2 is the $PLAYLISTS index,
+# $3 is the type ($MUSIC or $VID)
+SET_MV_ARRAY () { MUSICVIDS_URLS[$1]="${MUSICVIDS_URLS[$1]}$3${PLAYLISTS[$2]}" ; } 
+
+# update the $DEV_URLS if $USER param exist
 UPDATE_DEV () {
-    # if the 2nd param exist
     if [ $USER ] ; then
-        # update dev url with user param
         DEV_URLS[0]="https://github.com/$USER"
         DEV_URLS[1]="https://gitstalk.netlify.com/$USER"
     fi
 }
 
-MUSIC () {
-    if [ $MUSIC_PLAYLIST = "lofi" ] ; then
-        if [ $LOFI = "-r" ] || [ $LOFI = "--relax" ] ; then MUSICVIDS_URLS[1]="${MUSICVIDS_URLS[1]}$VID${LISTS[3]}"
-        elif [ $LOFI = "-s" ] || [ $LOFI = "--sleep" ] ; then MUSICVIDS_URLS[1]="${MUSICVIDS_URLS[1]}$VID${LISTS[4]}"
+# update the $MUSICVIDS_URLS if $PLAYLIST_TYPE param exist
+UPDATE_MUSICVIDS () {
+    if [ $PLAYLIST_TYPE = "xx" ] ; then SET_MV_ARRAY 0 0 $MUSIC
+    elif [ $PLAYLIST_TYPE = "sleep" ] ; then SET_MV_ARRAY 0 1 $MUSIC
+    elif [ $PLAYLIST_TYPE = "dope" ] ; then SET_MV_ARRAY 0 2 $MUSIC
+    elif [ $PLAYLIST_TYPE = "lofi" ] ; then
+        if [ $LOFI = "-r" ] || [ $LOFI = "--relax" ] ; then SET_MV_ARRAY 1 3 $VID
+        elif [ $LOFI = "-s" ] || [ $LOFI = "--sleep" ] ; then SET_MV_ARRAY 1 4 $VID
         fi
-    elif [ $MUSIC_PLAYLIST = "xx" ] ; then MUSICVIDS_URLS[0]="${MUSICVIDS_URLS[0]}$LIST${LISTS[0]}"
-    elif [ $MUSIC_PLAYLIST = "sleep" ] ; then MUSICVIDS_URLS[0]="${MUSICVIDS_URLS[0]}$LIST${LISTS[1]}"
-    elif [ $MUSIC_PLAYLIST = "dope" ] ; then MUSICVIDS_URLS[0]="${MUSICVIDS_URLS[0]}$LIST${LISTS[2]}"
     fi
 }
-
-# will open new tabs in chrome with given arguments
-CHR () { open -a "Google Chrome" $* ; }
-
-VID="watch?v="
-LIST="playlist?list="
-
-# First parameter
-PARAM=$1
 
 # if the param exist
 if [ $PARAM ] ; then
@@ -80,15 +95,15 @@ if [ $PARAM ] ; then
     # you got the logic for the 4 next cases
     elif [ $PARAM = "-s" ] || [ $PARAM = "--social" ] ; then CHR ${SOCIAL_URLS[*]}
     elif [ $PARAM = "-mv" ] || [ $PARAM = "--musicvids" ] ; then
-        MUSIC_PLAYLIST=$2
+        PLAYLIST_TYPE=$2
         LOFI=$3
-        MUSIC
+        UPDATE_MUSICVIDS
         CHR ${MUSICVIDS_URLS[*]}
     elif [ $PARAM = "-w" ] || [ $PARAM = "--work" ] ; then CHR ${MAIL_URLS[*]} ${DEV_URLS[*]}
     elif [ $PARAM = "-c" ] || [ $PARAM = "--chill" ] ; then 
-        MUSIC_PLAYLIST=$2
+        PLAYLIST_TYPE=$2
         LOFI=$3
-        MUSIC
+        UPDATE_MUSICVIDS
         CHR ${SOCIAL_URLS[*]} ${MUSICVIDS_URLS[*]}
     # else if the param is -a or --all
     elif [ $PARAM = "-a" ] || [ $PARAM = "--all" ] ; then
