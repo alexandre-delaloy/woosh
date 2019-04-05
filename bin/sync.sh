7dd7d3930be0367f2ab9e4879dbcdf1454d0c4f1
blyndusk <alexandredelaloydev@gmail.com>
2019-04-05 17:21:56 +0200

    :boom: SYNC & UPDATE fn() are now located in sync.sh

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # shellcheck disable=SC1091
+# shellcheck disable=SC2181
 
 function SYNC () {
     # for every file in this repository,
@@ -33,4 +34,22 @@ function SYNC () {
         --exclude "update.sh" \
         -avh --no-perms . ~;
     . ~;
+}
+
+function UPDATE () {
+    if [[ "$1" == "-f" || "$1" == "--force" ]] ; then SYNC ;
+    else
+        clear ; read -r -p  "Overwrite existing files ? (y/n) " -n 1 ;
+        if [ $? -eq 0 ]; then clear
+            if [[ $REPLY =~ ^[Yy]$ ]] ; then SYNC ;
+            elif [[ $REPLY =~ ^[Nn]$ ]] ; then echo "Execution stopped"
+            else echo -e "\e[38;5;1m$REPLY \e[38;5;15misn't an available value !" ; fi 
+        else
+            clear ; RETURNED="$1"
+            echo "Overwrite existing files ? (y/n)" ; read -r RETURNED ; clear
+            if [[ $RETURNED == "Y" ||  $RETURNED == "y" ]] ; then SYNC ;
+            elif [[ $RETURNED == "N" ||  $RETURNED == "n" ]] ; then echo "Execution stopped"
+            else echo -e "\e[38;5;1m$RETURNED \e[38;5;15misn't an available value !" ; fi 
+        fi
+    fi
 }
