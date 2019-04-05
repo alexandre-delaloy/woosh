77188d098245741b4e8909c037234c17dda2e3ab
blyndusk <alexandredelaloydev@gmail.com>
2019-04-05 17:00:34 +0200

    :wench: update update.sh to bash & zsh shell

--- update.sh
+++ update.sh
@@ -1,5 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# shellcheck disable=SC1091
 
 . bin/sync.sh
 
-SYNC
+
+if [[ "$1" == "-f" || "$1" == "--force" ]] ; then SYNC ;
+else
+    clear ; read -r -p  "Overwrite existing files ? (y/n) " -n 1 ;
+    if [ $? -eq 0 ]; then clear
+        if [[ $REPLY =~ ^[Yy]$ ]] ; then SYNC ;
+        elif [[ $REPLY =~ ^[Nn]$ ]] ; then echo "Execution stopped"
+        else echo -e "\e[38;5;1m$REPLY \e[38;5;15misn't an available value !" ; fi 
+    else
+        clear ; RETURNED="$1"
+        echo "Overwrite existing files ? (y/n)" ; read -r RETURNED ; clear
+        if [[ $RETURNED == "Y" ||  $RETURNED == "y" ]] ; then SYNC ;
+        elif [[ $RETURNED == "N" ||  $RETURNED == "n" ]] ; then echo "Execution stopped"
+        else echo -e "\e[38;5;1m$RETURNED \e[38;5;15misn't an available value !" ; fi 
+    fi
+fi;
