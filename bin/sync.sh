fe195f7a8b344febe826db43bdead346b7734ecc
blyndusk <alexandredelaloydev@gmail.com>
2019-04-05 16:59:17 +0200

    :lipstick: add UI to sync command

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
+# shellcheck disable=SC1091
 
 function SYNC () {
     # for every file in this repository,
     for i in "$(pwd)"/.*
     do
         # return the same file in the home directory
-        FILE=$HOME/$(basename "$i")
+        BASEI=$(basename "$i")
+        FILE=$HOME/$BASEI
         # if this file don't exist and if its not specific file,
         if [[ ! -f "$FILE" 
             && "$FILE" != "$HOME/.git"
             && "$FILE" != "$HOME/."
             && "$FILE" != "$HOME/.." ]]; then
+            # echo that the file isn't founded
+            echo -e "$BASEI \e[38;5;1m=> not found \e[38;5;15m"
             # create the file
-            echo -e "$FILE \e[38;5;196m not found \e[38;5;15m"
-            touch "$FILE" ; echo -e "$FILE \e[38;5;47m created ! \e[38;5;15m"
+            touch "$FILE" ; echo -e "$FILE \e[38;5;2m=> created \e[38;5;15m"
+        else 
+            # echo taht the file exist
+            echo -e "$BASEI \e[38;5;3m=> found \e[38;5;15m"
         fi
-    done
+    done ; sleep 1
     # sync all files from this repo to my computer, excluding specific files
-    rsync --exclude "bin" \
-        --exclude "scripts" \
-        --exclude ".git" \
+    rsync --exclude "bin/" \
+        --exclude "scripts/" \
+        --exclude ".git/" \
         --exclude ".zshrc" \
         --exclude "install.sh" \
         --exclude "LICENSE" \
         --exclude "README.md" \
         --exclude "update.sh" \
         -avh --no-perms . ~;
-    # shellcheck disable=SC1091
     . ~;
 }
