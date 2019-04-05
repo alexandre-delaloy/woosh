191b829f862808ac5103d873fe84b872a90f4324
blyndusk <alexandredelaloydev@gmail.com>
2019-04-05 16:27:06 +0200

    :lipstick: add colors to SYNC fn()

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 function SYNC () {
-    # for every file in my dotfiles repository
+    # for every file in this repository,
     for i in "$(pwd)"/.*
     do
         # return the same file in the home directory
         FILE=$HOME/$(basename "$i")
-        # if this file not exist or if its not a git file,
-        if [[ ! -f "$FILE" && "$FILE" != "$HOME/.git" ]]; then
+        # if this file don't exist and if its not specific file,
+        if [[ ! -f "$FILE" 
+            && "$FILE" != "$HOME/.git"
+            && "$FILE" != "$HOME/."
+            && "$FILE" != "$HOME/.." ]]; then
             # create the file
-            echo "$FILE not found !" && touch "$FILE"
+            echo -e "$FILE \e[38;5;196m not found \e[38;5;15m"
+            touch "$FILE" ; echo -e "$FILE \e[38;5;47m created ! \e[38;5;15m"
         fi
     done
-    # sync all files in my dotfiles repository, excluding specific files
-    rsync --exclude ".git" \
+    # sync all files from this repo to my computer, excluding specific files
+    rsync --exclude "bin" \
+        --exclude "scripts" \
+        --exclude ".git" \
         --exclude ".zshrc" \
+        --exclude "install.sh" \
         --exclude "LICENSE" \
         --exclude "README.md" \
-        --exclude "bin" \
-        --exclude "scripts" \
-        --exclude "install.sh" \
         --exclude "update.sh" \
         -avh --no-perms . ~;
+    # shellcheck disable=SC1091
     . ~;
 }
