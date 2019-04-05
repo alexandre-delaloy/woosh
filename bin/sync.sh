c59632060a751b98bdef32d8d093e3df3f81e1d4
blyndusk <alexandredelaloydev@gmail.com>
2019-04-05 18:04:58 +0200

    :recycle: update UPDATE fn(), using vared insted of read in zsh

--- bin/sync.sh
+++ bin/sync.sh
@@ -39,14 +39,14 @@ function SYNC () {
 function UPDATE () {
     if [[ "$1" == "-f" || "$1" == "--force" ]] ; then SYNC ;
     else
-        clear ; read -r -p  "Overwrite existing files ? (y/n) " -n 1 ;
+        clear ; read -r -p "Overwrite existing files ? (y/n) " -n 1 ;
         if [ $? -eq 0 ]; then clear
             if [[ $REPLY =~ ^[Yy]$ ]] ; then SYNC ;
             elif [[ $REPLY =~ ^[Nn]$ ]] ; then echo "Execution stopped"
             else echo -e "\e[38;5;1m$REPLY \e[38;5;15misn't an available value !" ; fi 
         else
-            clear ; RETURNED="$1"
-            echo "Overwrite existing files ? (y/n)" ; read -r RETURNED ; clear
+            RETURNED=""
+            clear ; vared -p "Overwrite existing files ? (y/n) " -c RETURNED ; echo ; clear
             if [[ $RETURNED == "Y" ||  $RETURNED == "y" ]] ; then SYNC ;
             elif [[ $RETURNED == "N" ||  $RETURNED == "n" ]] ; then echo "Execution stopped"
             else echo -e "\e[38;5;1m$RETURNED \e[38;5;15misn't an available value !" ; fi 
