1467a9b23c61202575985f7f719856ac76e89f0c
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-02-02 23:37:25 +0100

    add: more colors

--- bin/ui.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-red()     { echo -e "\e[1;30;41m$1\e[0m"; }
-green()   { echo -e "\e[1;30;42m$1\e[0m"; }
-yellow()  { echo -e "\e[1;30;43m$1\e[0m"; }
-blue()    { echo -e "\e[1;34m$1\e[0m"; }
-magenta() { echo -e "\e[1;30;45m$1\e[0m"; }
-cyan()    { echo -e "\e[1;30;46m$1\e[0m"; }
