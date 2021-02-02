cf1142d19d1c781a57b969f6705e8f864d2248b5
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-02-02 23:42:02 +0100

    fix: apt installs

--- bin/apt.sh
+++ bin/apt.sh
@@ -2,11 +2,23 @@
 
 source bin/fns.sh
 
-RUN_COMMAND echo "Oyy"
-RUN_COMMAND sudo apt upgrade
+which "sudo" > /dev/null 2>&1
+if [[ "$?" = 0 ]] ; then
+  ISUDO="sudo"
+else
+  ISUDO=""
+fi
+# source bin/packages/gh.sh
+# source bin/packages/yarn.sh
+
+RUN_COMMAND $ISUDO apt update
+RUN_COMMAND $ISUDO apt upgrade
 
 INSTALL_PACKAGE curl
 INSTALL_PACKAGE git
+INSTALL_PACKAGE gh
 INSTALL_PACKAGE htop
 INSTALL_PACKAGE shellcheck
 INSTALL_PACKAGE xclip
+INSTALL_PACKAGE yarn
+INSTALL_PACKAGE zsh
