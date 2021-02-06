5a3ac97c02fd4d0c821298905e3444c98d6e316d
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-02-05 19:48:24 -0600

    add: cool retro term config json

--- bin/snaps.sh
+++ bin/snaps.sh
@@ -2,18 +2,31 @@
 
 source bin/utils.sh
 
+function INSTALL_CHAT_SNAPS() {
+  sudo snap install slack
+  CHECK_COMMAND_INSTALLATION slack
+
+  sudo snap install teams
+  CHECK_COMMAND_INSTALLATION teams
+
+  sudo snap install telegram-desktop
+  CHECK_COMMAND_INSTALLATION telegram-desktop
+
+  sudo snap install discord
+  CHECK_COMMAND_INSTALLATION discord
+}
+function INSTALL_DEV_SNAPS() {
+  sudo snap install kubectl --classic 
+  CHECK_COMMAND_INSTALLATION kubectl
+
+  
+}
+
 if [ $1 = "4.1" ] ; then
   sudo snap install kubectl --classic 
   CHECK_COMMAND_INSTALLATION kubectl
 elif [ $1 = "4.2" ] ; then
-  sudo snap install slack
-  CHECK_COMMAND_INSTALLATION slack
-
-  sudo snap install discord
-  CHECK_COMMAND_INSTALLATION discord
-
-  sudo snap install telegram-desktop
-  CHECK_COMMAND_INSTALLATION telegram-desktop
+  INSTALL_CHAT_SNAPS
 else
   sudo snap install kubectl --classic 
   CHECK_COMMAND_INSTALLATION kubectl
@@ -24,3 +37,14 @@ else
   sudo snap install telegram-desktop
   CHECK_COMMAND_INSTALLATION telegram-desktop
 fi
+
+# vscode
+# discord
+# gimp
+# lens
+# kubectl
+# postman
+# slack
+# spotify
+# telegram-desktop
+# vlc
