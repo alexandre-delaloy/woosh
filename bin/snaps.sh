da77e9a9bea56e6ee8e4f162feb80baa7e5e1251
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-02-08 12:42:49 +0100

    add: snap package for dev, chat & misc

--- bin/snaps.sh
+++ bin/snaps.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+# slack --classic
+# teams
+# telegram-desktop
+# discord
+
+# code --classic
+# cool-retro-term
+# kubectl --classic
+# kontena-lens --classic
+# postman 
+# arduino 
+
+# vlc
+# gimp
+
 source bin/utils.sh
 
 function INSTALL_CHAT_SNAPS() {
-  sudo snap install slack
+  sudo snap install slack --classic
   CHECK_COMMAND_INSTALLATION slack
 
   sudo snap install teams
@@ -16,35 +31,41 @@ function INSTALL_CHAT_SNAPS() {
   CHECK_COMMAND_INSTALLATION discord
 }
 function INSTALL_DEV_SNAPS() {
+  sudo snap install code --classic 
+  CHECK_COMMAND_INSTALLATION code
+
+  sudo snap install cool-retro-term --classic 
+  CHECK_COMMAND_INSTALLATION cool-retro-term
+  
   sudo snap install kubectl --classic 
   CHECK_COMMAND_INSTALLATION kubectl
 
+  sudo snap install kontena-lens --classic 
+  CHECK_COMMAND_INSTALLATION kontena-lens
+
+  sudo snap install postman 
+  CHECK_COMMAND_INSTALLATION postman
+
+  sudo snap install arduino 
+  CHECK_COMMAND_INSTALLATION arduino
+}
+
+function INSTALL_MISC_SNAPS() {
+  sudo snap install vlc
+  CHECK_COMMAND_INSTALLATION vlc
   
+  sudo snap install gimp
+  CHECK_COMMAND_INSTALLATION gimp
 }
 
 if [ $1 = "4.1" ] ; then
-  sudo snap install kubectl --classic 
-  CHECK_COMMAND_INSTALLATION kubectl
+  INSTALL_DEV_SNAPS
 elif [ $1 = "4.2" ] ; then
   INSTALL_CHAT_SNAPS
+elif [ $1 = "4.3" ] ; then
+  INSTALL_MISC_SNAPS
 else
-  sudo snap install kubectl --classic 
-  CHECK_COMMAND_INSTALLATION kubectl
-
-  sudo snap install discord
-  CHECK_COMMAND_INSTALLATION discord
-
-  sudo snap install telegram-desktop
-  CHECK_COMMAND_INSTALLATION telegram-desktop
+  INSTALL_DEV_SNAPS
+  INSTALL_CHAT_SNAPS
+  INSTALL_MISC_SNAPS
 fi
-
-# vscode
-# discord
-# gimp
-# lens
-# kubectl
-# postman
-# slack
-# spotify
-# telegram-desktop
-# vlc
--- woo.sh
+++ woo.sh
@@ -12,8 +12,9 @@ woosh() {
     $(yellow "[2]") Install $(fyellow "apt packages")
     $(yellow "[3]") Copy $(fyellow "dotfiles") (.aliases, .functions, etc..)
     $(yellow "[4]")   Install $(fgreen "all") $(fyellow "snap packages")
-    $(yellow "[4.1]") Install $(fgreen "[dev]") $(fyellow "snap packages") (vscode, terminator, postman, kubectl, lens)
-    $(yellow "[4.2]") Install $(fgreen "[chat]") $(fyellow "snap packages") (slack, teams, telegram-desktop, discord)
+    $(yellow "[4.1]") Install $(fgreen "[dev]") $(fyellow "snap packages") (vscode, kubectl, lens, postman, arduino, cool retro term)
+    $(yellow "[4.2]") Install $(fgreen "[chat]") $(fyellow "snap packages") (slack, teams, telegram, discord)
+    $(yellow "[4.3]") Install $(fgreen "[misc]") $(fyellow "snap packages") (vlc, gimp)
     -
     $(red "[0]") $(fred "Exit")
 
@@ -45,6 +46,10 @@ woosh() {
     echo "
       "
     source bin/snaps.sh 4.2
+  elif [[ $NUMBER = "4.3" ]] ; then
+    echo "
+      "
+    source bin/snaps.sh 4.3
   else
       return 0
   fi
