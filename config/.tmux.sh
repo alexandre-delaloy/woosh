6b5a7adb4a64215b05be28d62f30f6c57bf69d76
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-02-08 16:41:24 +0100

    fix: tmux session

--- config/.tmux.sh
+++ config/.tmux.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
 tmux new-session \; \
+  kill-session -a \; \
   send-keys 'htop' C-m \; \
   split-window -v -p 66 \; \
   split-window -h -p 50 \; \
   send-keys 'cd ~/_/CONFIG/dotfiles ; bash woo.sh' C-m \; \
   select-pane -t 1 \; \
-  send-keys 'clear ; cd ; date ; printf "\n" ; hostnamectl' C-m \; \
+  send-keys 'cd ; date ; printf "\n" ; hostnamectl' C-m \; \
   split-window -v \; \
   send-keys 'cd ~/_ ; clear ; ls -l' C-m \; \
-  setw -g mouse on
+  setw -g mouse on \; \
