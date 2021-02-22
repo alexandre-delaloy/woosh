92da5c756f1f4d1fbed1abff431ef0f2858bf00d
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-02-22 19:08:46 +0100

    add: zshrc writing confirmation prompt

--- bin/cp.sh
+++ bin/cp.sh
@@ -2,12 +2,22 @@
 
 source bin/utils.sh
 
+function update_zsh_file() {
+  read -p "$(yellow 'Update zsh file ? [Y/n]') " -n 1 -r YN
+  echo ; echo  # (optional) move to a new line
+if [[ $YN =~ ^[Yy]$ ]] ; then
+  echo "
+source ~/.aliases
+bash ~/.tmux.sh
+  " >> ~/.zshrc
+  else
+    return 0
+  fi
+}
+
 COPY_FILE config/ .aliases ~
 COPY_FILE config/ .functions ~
 COPY_FILE config/ .blyndusk-theme.cool-retro-theme.json ~
 COPY_FILE config/ .tmux.sh ~
 
-echo "
-source ~/.aliases
-bash ~/.tmux.sh
-" >> ~/.zshrc
+update_zsh_file
